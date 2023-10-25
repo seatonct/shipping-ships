eb490cf9f97e0f7817bcf1aa528b4f45b62bb0af
Charles Seaton <seatonct@gmail.com>
2023-10-25 14:37:22 -0500

    added stuff

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+export const NavBar = () => {
+  return (
+    <ul className="navbar">
+      <li className="navbar_item">
+        <Link to="/ShippingShips">Ships</Link>
+      </li>
+      <li className="navbar_item">
+        <Link to="/haulingships">Haulers</Link>
+      </li>
+      <li className="navbar_item">Docks</li>
+    </ul>
+  );
+};
---
+++ src/services/shipService.js
@@ -0,0 +1,11 @@
+export const getAllShips = () => {
+  return fetch(`http://localhost:8000/ships?_expand=hauler`).then((res) =>
+    res.json()
+  );
+};
+
+export const deleteShip = (shipId) => {
+  return fetch(`http://localhost:8000/ships/${shipId}`, {
+    method: "DELETE",
+  });
+};
